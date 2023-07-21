644c8e79e0c2d12079c3b1723387002228dc365f
Huixin Zhan <80724524+huixin-zhan@users.noreply.github.com>
2023-07-20 18:42:02 -0700

    Updates files

--- test_zhanh.sh
+++ test_zhanh.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-#$ -pe smp 4
 #$ -N job_test_zhanh
 #$ -cwd
 #$ -q 1tb.q
-#$ -o /common/zhanh/out_test
-#$ -e /common/zhanh/error_test
+#$ -o /common/zhanh/hpc_testing_zhanh/out_test
+#$ -e /common/zhanh/hpc_testing_zhanh/error_test
 source /common/zhanh/anaconda3/etc/profile.d/conda.sh
-conda activate <myenv>
+conda activate tf_test_zhanh
 /common/zhanh/anaconda3/envs/tf_test_zhanh/bin/python
 python \
-/common/zhanh/hpc_testing/tensorflow_testing.py \
---index 3 \
---template 'False' \
---population 100 \
---generations 100 \
---njobs 16
+/common/zhanh/hpc_testing_zhanh/tensorflow_testing.py
